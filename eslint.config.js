ea67bb7a2e4f9a6cd9a4cc4fd1f6de5c71aab753
siaka.d <130613363+chacka1315@users.noreply.github.com>
2025-09-02 18:27:20 +0000

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import globals from 'globals';
+export default defineConfig([
+  {
+    files: ['**/*.js'],
+    plugins: {
+      js,
+    },
+    extends: ['js/recommended'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'no-var': 'error',
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        myCustomGlobal: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
+  eslintConfigPrettier,
+]);
